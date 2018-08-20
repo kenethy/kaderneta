21c30acd02bac959f10947bea735540f0488ee4d
Siga Mais <Siga Mais@DESKTOP-MQ0DKDS>
2018-08-19 22:10:28 -0300

    Alterações Notes e Registration

--- src/main/java/com/demo/kaderneta/api/NotesController.java
+++ src/main/java/com/demo/kaderneta/api/NotesController.java
@@ -3,7 +3,10 @@ package com.demo.kaderneta.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,7 @@ import com.demo.kaderneta.repository.NotesRepository;
 
 @RestController
 @RequestMapping("/notes")
+@CrossOrigin
 public class NotesController {
 
 	@Autowired
@@ -23,5 +27,8 @@ public class NotesController {
 		return notesRepository.findAll();
 	}
 	
-	
+	@PostMapping(path = "/add")
+	public Notes save(@RequestBody Notes notes) {
+		return notesRepository.save(notes);
+	}
 }
--- src/main/java/com/demo/kaderneta/api/RegistrationController.java
+++ src/main/java/com/demo/kaderneta/api/RegistrationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.demo.kaderneta.repository.RegistrationRepository;
 
 @RestController
 @RequestMapping("/registration")
+@CrossOrigin
 public class RegistrationController {
 
 	@Autowired
